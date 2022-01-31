c26fe455cd37c89561146b9638c0e6fedba15fff
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2022-01-31 20:53:09 +0530

    Added basic bootstrap styling

--- seeds/index.js
+++ seeds/index.js
@@ -20,11 +20,16 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   
   for(let i = 0; i < 50; i++) {
-    const  random1000 = Math.floor(Math.random() * 1000);
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 9;
+
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`
-    })
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, sint nobis aut, quasi laudantium voluptas sunt ut aliquam necessitatibus numquam fugiat exercitationem nulla ab facere quaerat at molestiae consectetur voluptates!',
+      price: price
+    });
     await camp.save();
   }
 };
