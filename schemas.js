dafb0542e5f2a515ad5162ca88a01fc0ccf93c0a
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2022-02-02 23:26:15 +0530

    Added reviews model and validation

--- schemas.js
+++ schemas.js
@@ -10,6 +10,14 @@ const campgroundSchema = Joi.object({
     }).required()
 });
 
+const reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        body: Joi.string().required()
+    }).required()
+});
+
 module.exports = {
     campgroundSchema,
+    reviewSchema,
 }
