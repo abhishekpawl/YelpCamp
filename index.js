fff434ede5e29011bdc67cc496d67880b73e040f
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2022-02-14 18:19:23 +0530

    Fixed register and login route

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ passport.deserializeUser(user.deserializeUser());
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
+    res.locals.currentUser = req.user;
     next();
 })
 
--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ router.get('/register', (req, res) => {
     res.render('users/register')
 })
 
-router.post('/register', catchAsync(async(req, res) => {
+router.post('/register', catchAsync(async(req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({
@@ -16,8 +16,13 @@ router.post('/register', catchAsync(async(req, res) => {
             username
         });
         const registeredUser = await User.register(user, password);
-        req.flash('success', 'Welcome to Yelp Camp!');
-        res.redirect('/campgrounds');
+        req.login(registeredUser, err => {
+            if (err) {
+                return next(err);
+            }
+            req.flash('success', 'Welcome to Yelp Camp!');
+            res.redirect('/campgrounds');
+        })
     } catch (e) {
         req.flash('error', 'Username or email already exists!');
         res.redirect('register');
@@ -33,7 +38,9 @@ router.post('/login', passport.authenticate('local', {
     failureRedirect: '/login'
 }), (req, res) => {
     req.flash('success', 'Welcome back!');
-    res.redirect('/campgrounds');
+    const redirectTo = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectTo);
 })
 
 router.get('/logout', (req, res) => {
