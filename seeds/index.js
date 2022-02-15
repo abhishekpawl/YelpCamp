01900eb84764144dd3e979568c2931b81a6f5056
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2022-02-15 11:26:34 +0530

    Added user to campground

--- seeds/index.js
+++ seeds/index.js
@@ -1,39 +1,40 @@
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const cities = require('./cities');
-const {places, descriptors} = require('./seedHelpers');
+const { places, descriptors } = require('./seedHelpers');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection error:"));
 db.once("open", () => {
-  console.log("Database connected");
+    console.log("Database connected");
 });
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
-const seedDB = async () => {
-  await Campground.deleteMany({});
-  
-  for(let i = 0; i < 50; i++) {
-    const random1000 = Math.floor(Math.random() * 1000);
-    const price = Math.floor(Math.random() * 20) + 9;
+const seedDB = async() => {
+    await Campground.deleteMany({});
 
-    const camp = new Campground({
-      location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'https://source.unsplash.com/collection/483251',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, sint nobis aut, quasi laudantium voluptas sunt ut aliquam necessitatibus numquam fugiat exercitationem nulla ab facere quaerat at molestiae consectetur voluptates!',
-      price: price
-    });
-    await camp.save();
-  }
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 9;
+
+        const camp = new Campground({
+            author: '620a36b5cfcd637b6673e142',
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, sint nobis aut, quasi laudantium voluptas sunt ut aliquam necessitatibus numquam fugiat exercitationem nulla ab facere quaerat at molestiae consectetur voluptates!',
+            price: price
+        });
+        await camp.save();
+    }
 };
 
 seedDB().then(() => {
-  mongoose.connection.close();
+    mongoose.connection.close();
 });
