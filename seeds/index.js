f769625ea43e16c6decc5034398202bb66881361
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2022-02-18 11:05:04 +0530

    Configured map popup

--- seeds/index.js
+++ seeds/index.js
@@ -29,6 +29,10 @@ const seedDB = async() => {
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, sint nobis aut, quasi laudantium voluptas sunt ut aliquam necessitatibus numquam fugiat exercitationem nulla ab facere quaerat at molestiae consectetur voluptates!',
             price: price,
+            geometry: {
+                type: "Point",
+                coordinates: [-113.1331, 47.0202]
+            },
             images: [
                 {
                     url: 'https://res.cloudinary.com/dd0xcevbq/image/upload/v1644995233/YelpCamp/lga5fenisjevgqeddgyf.jpg',
