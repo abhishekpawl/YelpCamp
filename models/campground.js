d450e9877008c75b67a297966f90ba0aedb9ca79
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2022-02-18 13:14:54 +0530

    Added cluster map

--- models/campground.js
+++ models/campground.js
@@ -11,6 +11,8 @@ ImageSchema.virtual('thumbnail').get(function() {
     return this.url.replace('/upload', '/upload/w_200');
 });
 
+const opts = {toJSON: {virtuals: true}};
+
 const CampgroundSchema = new Schema({
     title: String,
     images: [
@@ -38,6 +40,10 @@ const CampgroundSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'Review'
     }]
+}, opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function() {
+    return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>`;
 });
 
 CampgroundSchema.post('findOneAndDelete', async function(campground) {
@@ -46,6 +52,6 @@ CampgroundSchema.post('findOneAndDelete', async function(campground) {
             _id: { $in: campground.reviews }
         })
     }
-})
+});
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
