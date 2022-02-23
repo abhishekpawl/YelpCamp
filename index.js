858bc6fa2c489881ddff5fd1484cf756f9a81975
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2022-02-23 18:48:02 +0530

    Setup mongo atlas

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const userRoutes = require('./routes/users');
 
+/* const dbUrl = process.env.DB_URL; */
+
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useUnifiedTopology: true
